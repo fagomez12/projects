668a841a422b4dd27806731bea91267c534dc984
Felipe A. Gomez Olaya <gomez1104ol@gmail.com>
2025-07-07 14:27:49 +0200

    Added link to repository

--- ADC128D818/adc128_fw.c
+++ ADC128D818/adc128_fw.c
@@ -21,7 +21,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  *
- * GitHub Repository: https://github.com/yourusername/your-repository
+ * GitHub Repository: https://github.com/fagomez12/projects.git
  */
 
 
